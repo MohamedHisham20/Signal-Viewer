7623bb7c2aa66bd47e185cc54c0380660f9fb6de
abdelrahman-alaa-10 <abdelrahmanalaa846@gmail.com>
2024-10-08 21:43:25 +0300

    Graph version 2. Chart View & HSB

--- Controllers/GraphController.py
+++ Controllers/GraphController.py
@@ -4,45 +4,37 @@ from PyQt5.QtCore import Qt, QTimer
 from PyQt5.QtGui import QPainter
 from PySide6 import QtCharts
 import numpy as np
-
-from ..test.test import Graph
+from GUI.Graph import Graph
         
 class GraphController:
     @staticmethod
     def construct_graph(graph:Graph):
-        GraphController.construct_chart(graph)
-    
-    @staticmethod
-    def construct_chart(graph:Graph):
-        graph.chart.addSeries(graph.series)
-        graph.chart.createDefaultAxes()
+        GraphController.init_layout(graph)
     
     @staticmethod
     def init_layout(graph:Graph):
-        """ Initialize graph layout as a QHBoxLayout and add its buttons"""    
-        graph.layout = QHBoxLayout(graph)
+        """ Initialize graph layout as a QHBoxLayout and append its children.
+        """    
+        graph._layout_.addWidget(graph.graph_controls)
         
-        graph.layout.addWidget(graph.name_label)
+        graph.graph_controls_layout.addWidget(graph.name_label)
+        graph.graph_controls_layout.addWidget(graph.change_name_btn)        
+        graph.graph_controls_layout.addWidget(graph.reset_btn)
+        graph.graph_controls_layout.addWidget(graph.delete_btn)
         
-        graph.layout.addWidget(graph.change_name_btn)
-        graph.change_name_btn.clicked.connect(graph.update_graph_name)
+        graph._layout_.addWidget(graph.chart_view)
+        graph.chart.addSeries(graph.series)
+        graph.chart.createDefaultAxes()
         
-        graph.layout.addWidget(graph.reset_btn)
-        graph.change_name_btn.clicked.connect(graph.reset_graph)
-        
-        graph.layout.addWidget(graph.delete_btn)
-        graph.delete_btn.clicked.connect(graph.delete_graph)
-
-     
+        graph._layout_.addWidget(graph.horizontal_scroll_bar)
+ 
     @staticmethod            
     def update_graph_name(graph:Graph, new_name:str):
         graph.name_label.setText(new_name) 
         
-    
     @staticmethod    
     def reset_graph(graph:Graph):
         pass
-    
     
     @staticmethod
     def delete_graph(graph:Graph):
--- test/test.py
+++ test/test.py
@@ -1,70 +1,26 @@
 from PySide6.QtWidgets import (QApplication, QMainWindow ,QWidget, QLabel, QVBoxLayout,
-    QHBoxLayout, QPushButton) 
-from PySide6 import QtCharts
+QHBoxLayout, QPushButton ,QScrollBar)
+from PySide6.QtCore import Qt 
+from PySide6.QtCharts import QChart, QChartView, QLineSeries
 import numpy as np
 import sys
-
-class Graph(QWidget):
-    def __init__(self, name = "graph1"):
-        """ Construct a Graph with name if given """
-        super().__init__()
-        #Graph member variables initialization
-        #Direct children of graph are label, btns and chart_view
-        
-        self._layout_ = QVBoxLayout(self)
-        
-        self.name_label = QLabel(name,self)
-        self.change_name_btn = QPushButton("change name",self)
-        self.reset_btn = QPushButton("reset",self)
-        self.delete_btn = QPushButton("delete Graph",self)
-        
-        self.chart = QtCharts.QChart()
-        self.chart_view = QtCharts.QChartView(self.chart,self)
-        self.series = QtCharts.QLineSeries()
-        
-        GraphController.construct_graph(self)
+import csv
   
-class GraphController:
-    @staticmethod
-    def construct_graph(graph:Graph):
-        GraphController.init_layout(graph)
-    
-    @staticmethod
-    def init_layout(graph:Graph):
-        """ Initialize graph layout as a QHBoxLayout and add its children"""    
-        graph._layout_.addWidget(graph.name_label)
-        graph._layout_.addWidget(graph.change_name_btn)        
-        graph._layout_.addWidget(graph.reset_btn)
-        graph._layout_.addWidget(graph.delete_btn)
-        graph._layout_.addWidget(graph.chart_view)
-        graph.chart.addSeries(graph.series)
-        graph.chart.createDefaultAxes()
- 
-    @staticmethod            
-    def update_graph_name(graph:Graph, new_name:str):
-        graph.name_label.setText(new_name) 
-        
-    @staticmethod    
-    def reset_graph(graph:Graph):
-        pass
-    
-    @staticmethod
-    def delete_graph(graph:Graph):
-        pass                 
+import GUI              
 
 class MainWindow(QMainWindow):
     def __init__(self,name:str):
         super().__init__()
         self.setGeometry(0,0,500,500)
         self.setWindowTitle(name)  
-        self.graph = Graph("Test")
+        self.graph = Graph("This Label should display the graph name")
         self.setCentralWidget(self.graph)
  
 def main():
     app = QApplication([])
     window = MainWindow("test window")
     window.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
 
       
 main()    
