dda72ac355c116bc74c7956fd8aaaa02f5796ca2
salahmohamed <salahmohamed.sm2016@gmail.com>
2024-10-18 20:15:49 +0300

    connections

--- Connections.py
+++ Connections.py
@@ -4,12 +4,9 @@ from PySide6 import QtWidgets
 
 def NonRect_connections(graph: NonRectGraph , ui: Ui_MainWindow):
     ui.Channels.setFixedHeight(400)
-    # Ensure nonRect_widget has a layout
     if ui.nonRect_widget.layout() is None:
         layout = QtWidgets.QHBoxLayout(ui.nonRect_widget)
-        # layout.setContentsMargins(0, 20, 0, 20)
         ui.nonRect_widget.setLayout(layout)
-
     graph.pause_radar()
     ui.nonRect_widget.layout().addWidget(graph)
     ui.stop_c4.clicked.connect(lambda: graph.pause_radar())
@@ -21,6 +18,5 @@ def NonRect_connections(graph: NonRectGraph , ui: Ui_MainWindow):
     ui.dial_slide_c4.setRange(0, 360)
     ui.dial_speed_c4.valueChanged.connect(lambda: graph.change_speed(ui.dial_speed_c4.value()))
     ui.dial_speed_c4.setRange(1, 20)
-
     return ui
     
--- main.py
+++ main.py
@@ -12,15 +12,5 @@ ui.Channels.setFixedHeight(400)
 
 NonRect_connections(NonRectGraph(ui.nonRect_widget), ui)
 
-# # Ensure nonRect_widget has a layout
-# if ui.nonRect_widget.layout() is None:
-#     layout = QtWidgets.QHBoxLayout(ui.nonRect_widget)
-#     # layout.setContentsMargins(0, 20, 0, 20)
-#     ui.nonRect_widget.setLayout(layout)
-
-# non_rect_graph = NonRectGraph(ui.nonRect_widget)
-
-# ui.nonRect_widget.layout().addWidget(non_rect_graph)
-
 MainWindow.show()
 sys.exit(app.exec_())
