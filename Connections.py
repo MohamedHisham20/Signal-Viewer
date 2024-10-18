502df67621d2ff841d6dc1aa7637815c7dac854b
salahmohamed <salahmohamed.sm2016@gmail.com>
2024-10-18 20:02:59 +0300

    radar connections

---
+++ Connections.py
@@ -0,0 +1,26 @@
+from MainWindow import Ui_MainWindow
+from NonRectGraphController import NonRectGraph
+from PySide6 import QtWidgets
+
+def NonRect_connections(graph: NonRectGraph , ui: Ui_MainWindow):
+    ui.Channels.setFixedHeight(400)
+    # Ensure nonRect_widget has a layout
+    if ui.nonRect_widget.layout() is None:
+        layout = QtWidgets.QHBoxLayout(ui.nonRect_widget)
+        # layout.setContentsMargins(0, 20, 0, 20)
+        ui.nonRect_widget.setLayout(layout)
+
+    graph.pause_radar()
+    ui.nonRect_widget.layout().addWidget(graph)
+    ui.stop_c4.clicked.connect(lambda: graph.pause_radar())
+    ui.play_c4.clicked.connect(lambda: graph.play_radar())
+    ui.replay_c4.clicked.connect(lambda: graph.rewind_radar())
+    ui.dial_slide_c4.valueChanged.connect(lambda: graph.scroll_radar(ui.dial_slide_c4.value()))
+    ui.dial_slide_c4.sliderPressed.connect(lambda: graph.pause_radar())
+    ui.dial_slide_c4.sliderReleased.connect(lambda: graph.play_radar())
+    ui.dial_slide_c4.setRange(0, 360)
+    ui.dial_speed_c4.valueChanged.connect(lambda: graph.change_speed(ui.dial_speed_c4.value()))
+    ui.dial_speed_c4.setRange(1, 20)
+
+    return ui
+    
--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PySide6 import QtCore, QtGui, QtWidgets
 from MainWindow import Ui_MainWindow
-
-
+from NonRectGraphController import NonRectGraph
+from Connections import NonRect_connections
 import sys
 app = QtWidgets.QApplication(sys.argv)
 MainWindow = QtWidgets.QMainWindow()
 ui = Ui_MainWindow()
 ui.setupUi(MainWindow)
+
+ui.Channels.setFixedHeight(400)
+
+NonRect_connections(NonRectGraph(ui.nonRect_widget), ui)
+
+# # Ensure nonRect_widget has a layout
+# if ui.nonRect_widget.layout() is None:
+#     layout = QtWidgets.QHBoxLayout(ui.nonRect_widget)
+#     # layout.setContentsMargins(0, 20, 0, 20)
+#     ui.nonRect_widget.setLayout(layout)
+
+# non_rect_graph = NonRectGraph(ui.nonRect_widget)
+
+# ui.nonRect_widget.layout().addWidget(non_rect_graph)
+
 MainWindow.show()
 sys.exit(app.exec_())
